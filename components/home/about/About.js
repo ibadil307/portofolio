5dfd1773dbf314389658d7b835fbb75488b92755
ibadil307 <43997372+ibadil307@users.noreply.github.com>
2023-12-19 23:11:38 +0800

    Update About.js

--- components/home/about/About.js
+++ components/home/about/About.js
@@ -13,34 +13,50 @@ export const About = () => {
         <div>
           <Reveal>
             <p className={`${styles.aboutText} ${styles.highlightFirstLetter}`}>
-              Hey! I&apos;m Bob, if you haven&apos;t already gathered that by
-              now. I&apos;m a painter turned software engineer from Daytona,
-              Florida. I specialize in the backend, primarily Node and Rust, but
-              love building with whatever tools are right for the job.
+              Halo! Perkenalkan, saya Muhammad Ibadil Mukhlasin. Saya adalah 
+              seorang mahasiswa aktif di Politeknik Negeri Tanah Laut, tengah 
+              menempuh studi di jurusan D4 Teknologi Rekayasa Komputer. 
             </p>
           </Reveal>
           <Reveal>
             <p className={styles.aboutText}>
-              I currently work for Google on Google Photos. I also toss in my ¢2
-              with the design systems teams from time to time (once an artist,
-              always an artist, amirite?).
+              Saya memiliki ketertarikan yang mendalam dalam bidang perangkat 
+              keras dan perangkat lunak. Sebagai mahasiswa, saya telah belajar dan 
+              memperoleh pemahaman yang kuat tentang instalasi, konfigurasi, dan 
+              pemeliharaan perangkat jaringan seperti router, switch, dan access point.
             </p>
           </Reveal>
           <Reveal>
             <p className={styles.aboutText}>
-              Outside of work, I still love to paint. Any given Sunday
-              you&apos;ll find me scribbling some happy clouds with my son ☁️ I
-              even teach courses online if you&apos;re looking to learn!
+              Saya juga telah mengembangkan keterampilan dalam menggunakan aplikasi dan 
+              melakukan pemecahan masalah pada sistem operasi. Selama studi saya, saya 
+              telah terlibat dalam berbagai proyek yang melibatkan implementasi jaringan 
+              komputer dan instalasi sistem operasi. Hal ini telah memberi saya kesempatan 
+              untuk mengembangkan pemahaman praktis tentang perangkat keras dan perangkat 
+              lunak serta kemampuan dalam menghadapi tantangan yang mungkin timbul.
             </p>
           </Reveal>
           <Reveal>
             <p className={styles.aboutText}>
-              I&apos;m passively looking for new positions where I can merge my
-              love for code with my love for the canvas. If you think
-              you&apos;ve got an opening that I might like, let&apos;s connect
-              🔗
+            Saya adalah seorang yang tekun, antusias, dan berorientasi pada detail. 
+            Saya senang mempelajari hal-hal baru dan selalu siap untuk menghadapi 
+            tantangan baru dalam bidang perangkat keras dan perangkat lunak. Saya 
+            percaya bahwa penggabungan pengetahuan teknis dan keterampilan interpersonal 
+            dapat membantu saya dalam meraih kesuksesan di dunia profesional. Saya 
+            senang berkolaborasi dengan orang-orang yang memiliki minat dan komitmen 
+            yang sama dalam bidang ini.
             </p>
           </Reveal>
+              <Reveal>
+              <p className={styles.aboutText}>
+              Jika Anda memiliki proyek menarik atau peluang kerja yang sesuai dengan 
+              keahlian saya, saya sangat berharap dapat berdiskusi lebih lanjut dan 
+              berkontribusi pada kemajuan perusahaan atau tim Anda. Terima kasih telah 
+              meluangkan waktu untuk membaca "About Me" saya. Jika Anda memiliki pertanyaan 
+              atau ingin berhubungan, jangan ragu untuk menghubungi saya. 
+              Terima kasih atas perhatian Anda.
+              </p>
+              </Reveal>
           <Reveal>
             <div className={styles.links}>
               <div className={styles.linksText}>
